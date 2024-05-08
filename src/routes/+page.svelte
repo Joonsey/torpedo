fe0ed8516a3cd1eeb17801302ba4e8883ea19b4c
Johannes <johannes.ronning@tietoevry.com>
2024-05-08 17:35:14 +0200

    added more content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content text-center">
+		<div class="">
+			<h1 class="text-5xl font-bold">Tired of dating duds? Tor-pedo your love life into overdrive!</h1>
+			<div class="m-7">Check your compatability with Tor! The knight of your dreams, hero of things, saviour of none.</div>
+			<div class="group indicator">
+				<span class="group indicator-item badge mask mask-heart bg-red-400 group-hover:scale-110 transition-transform duration-300"> </span>
+				<button class="group btn btn-primary">Get Started!!</button>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="flex flex-col w-full">
+<div class="divider divider-primary"></div>
+</div>
+
+<div class="w-full h-screen items-center justify-center flex flex-col">
+	<h1 class="text-4xl font-bold p-16">Compatabilty test</h1>
+
+	<div class="stack">
+		<div class="artboard artboard-horizontal phone-5 bg-base-200 rounded shadow-2xl flex flex-col w-full lg:flex-row">
+			<div class="grid flex-grow m-10 card bg-primary-100 rounded-box place-items-center">content</div>
+			<div class="divider lg:divider-horizontal primary ">OR</div>
+			<div class="grid flex-grow m-10 card bg-primary-100 rounded-box place-items-center">content</div>
+		</div>
+		<div class="artboard artboard-horizontal bg-base-300 phone-5 bg-accent rounded shadow-2xl"></div>
+	</div>
+</div>
